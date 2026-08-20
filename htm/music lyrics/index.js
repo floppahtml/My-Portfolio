e7045083799af47d4c030f4928ef13562173ef7e
floppahtml <150704359+floppahtml@users.noreply.github.com>
2026-08-20 08:03:19 -0500

    Add files via upload

---
+++ htm/music lyrics/index.js
@@ -0,0 +1,82 @@
+function openPage(){
+    var x = document.getElementById("search").value;
+    x = x.toLowerCase();
+    if (x === "currents"){
+        window.open("./Pages/Currents.html");
+    }
+
+    if (x === "a very 1999 christmas"){
+        window.open("./pages/av1999(d).html")
+    }
+
+    if (x === "blurryface"){
+        window.open("./Pages/blurryface.html")
+    }
+
+    if (x === "bella ciao"){
+        window.open("./pages/Bella_Ciao.html")
+    }
+
+    if (x === "виртуальная любовь"){
+        window.open("./Pages/Виртуальная_любовь.html")
+    }
+
+    if (x === "virtual love"){
+        window.open("./Pages/Виртуальная_любовь.html")
+    }
+
+    if (x === "судно"){
+        window.open("./Pages/Судно.html")
+    }
+
+    if (x === "sudno"){
+        window.open("./Pages/Судно.html")
+    }
+
+    if (x === "chromakopia"){
+        window.open("./Pages/chromakopia.html")
+    }
+
+    if (x === "damn"){
+        window.open("./Pages/damn.html")
+    }
+
+    if (x === "damn."){
+        window.open("./Pages/damn.html")
+    }
+    if(x === "dont tap the glass"){
+        window.open("./Pages/dont_tap_the_glass.html")
+    }
+    if(x === "don't tap the glass"){
+        window.open("./Pages/dont_tap_the_glass.html")
+    }
+    if(x === "sublime"){
+        window.open("./Pages/sublime.html")
+    }
+    if(x === "until the sun explodes"){
+        window.open("./Pages/until_the_sun_explodes.html")
+    }
+//singles  
+    if (x === "chicago"){
+        window.open("./Pages/chicago.html");
+    }  
+    if (x === "fahrradsattel"){
+        window.open("./Pages/fahrradsattel.html")
+    }
+    if (x === "bicycle seat"){
+        window.open("./Pages/fahrradsattel.html")
+    }
+    if (x === "magnetic"){
+        window.open("./Pages/magnetic.html")
+    }
+    if(x === "freak on a leash"){
+        window.open("./Pages/freak_on_a_leash.html")
+    }
+    if(x === "in too deep"){
+        window.open("./Pages/in_too_deep.html")
+    }
+}
+
+//ill toss some usefull stuff i need to get atributed after this
+//window.alert(`You cant leave the search area blank`);
+//shadow hoshi
