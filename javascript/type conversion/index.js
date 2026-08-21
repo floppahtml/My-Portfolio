3de723b97d9e4eca2a0bc75dd94110c2a8a42296
floppahtml <150704359+floppahtml@users.noreply.github.com>
2026-08-21 14:37:47 -0500

    Add files via upload

---
+++ javascript/type conversion/index.js
@@ -0,0 +1,22 @@
+// type conversion = change the datatype of a variable to another
+//                   (strings, numbers, booleans)
+
+/*let age = window.prompt("How old are you?");
+//here we go with the type
+age = Number(age);
+
+age+=1;
+
+console.log(age, typeof age);*/
+
+let x = "shibby";
+let y = "shibby";
+let z = "shibby";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x);
+console.log(y);
+console.log(z);
