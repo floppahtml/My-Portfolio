05d5bf9143d096248a7aeb0bb7c1c96743cbdf13
floppahtml <150704359+floppahtml@users.noreply.github.com>
2026-08-21 14:31:32 -0500

    Add files via upload

---
+++ javascript/Basics/index.js
@@ -0,0 +1,7 @@
+//only works if you have dev tools and i do not -_-
+console.log(`shibby!`);
+//alert window we love to see them
+window.alert(`SHIBBY ALLERT`);
+//these are to take text boxes with something in them and use their ID to change wut they say
+document.getElementById("h1").textContent = `shibby`;
+document.getElementById("pee").textContent = `suck my dick fa****!!!`;
