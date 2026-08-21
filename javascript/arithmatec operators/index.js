9d2660b5bf7116a8f9bc81733b57aa88530ae44b
floppahtml <150704359+floppahtml@users.noreply.github.com>
2026-08-21 14:35:05 -0500

    Add files via upload

---
+++ javascript/arithmatec operators/index.js
@@ -0,0 +1,26 @@
+//arithmetic operators = operands (values, variables, etc.)
+//                       operaters (+ - * /)
+//                       ex. 11 = x + 5;
+//                       exponents indicated by **
+
+let students = 35;
+
+// addition students = students + 1;
+// subtraction students = students - 1;
+// multiplication students = students * 2;
+// division students = students / 10;
+// exponent students = students ** 2
+// modulus is the remainder of a divide students = students % 2;
+
+//augmented assignment opperaters basicly quicker ways to do what was done abbove
+//students += 1
+// subtraction students -= 1;
+// multiplication students *= 2;
+// division students /= 10;
+// exponents students **= 2;
+// modulated students %=2;
+// decrements which are weird go like this
+// something ++ is plus 1 to whatever it was vice versa for --
+
+//also this all follows pemdas
+document.getElementById(`pee pee`).textContent = students;
