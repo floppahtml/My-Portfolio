326098fce5e9c969bde84b9c2d954a7ad542914f
floppahtml <150704359+floppahtml@users.noreply.github.com>
2026-08-24 08:18:48 -0500

    Add files via upload

---
+++ javascript/string methods/index.js
@@ -0,0 +1,29 @@
+// string methods = allow you to manipulate and work with text (strings
+
+//let username = "Jake Schnider";
+
+//h3.textContent  = username.indexOf("S");
+//h3.textContent  = username.lastIndexOf("S");
+//h32.textContent  = username.charAt("4");
+//h3.textContent = username.length
+//username = username.trim();
+//username = username.toUpperCase();
+//username = username.toLowerCase();
+//username = username.repeat(3);
+//let result = username.startsWith(" ");
+//let result = username.endsWith(" ");
+//let result = username.includes(" ");
+/*if(result){
+    h3.textContent  = `Your Username cannot contain spaces`;
+}
+
+else{
+    h3.textContent  = username;
+}*/
+
+//let phnnum = "132-123-1234";
+
+//phnnum = phnnum.replaceAll(`-`,`/`);
+//phnnum = phnnum.padStart(20, 0);
+//phnnum = phnnum.padEnd(20, 0);
+//h3.textContent = phnnum
