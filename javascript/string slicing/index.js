77767b697fddc9aa9f9c1091614779c9e9c98825
floppahtml <150704359+floppahtml@users.noreply.github.com>
2026-08-24 08:20:01 -0500

    Add files via upload

---
+++ javascript/string slicing/index.js
@@ -0,0 +1,6 @@
+// string slicing = creating a substring from a portion of another string
+//                  string.slice(start, end)
+
+let fullname = "Jake Schnieder"
+let firstname = fullname.slice(0,4);
+h3.textContent = firstname;
